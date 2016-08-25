51276d3e26c7f90e0f4cc7c2a3a83d2fabe8f5dc
Adam Jobin <jobin.adam@gmail.com>
2016-08-25 17:33:49 -0500

    Commit

--- signup/main.py
+++ signup/main.py
@@ -1,25 +1,87 @@
-#!/usr/bin/env python
+
 #
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+import webapp2, re
+page_header="""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup</title>
+    <style type="text/css">
+        p.error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+"""
+
+page_footer="""
+</body>
+</html>
+"""
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        name_form="""
+        <form action="/name" method="post">
+            <label for="username">Username</label>
+                <input name="name" type="text">
+                <br>
+            <label for="password">Password</label>
+                <input name="password" type="password">
+                <br>
+            <label for="verify">Verify Password</label>
+                <input name="verify" type="password">
+                <br>
+            <label for="email">Email(Optional)</label>
+                <input name="email" type="email">
+                <br>
+                <input type="submit">
+        </form>
+        """
+        self.response.write(page_header + name_form + page_footer)
+
+class Response(webapp2.RequestHandler):
+    def post(self):
+        username= self.request.get("name")
+        password1= self.request.get("password")
+        verify1= self.request.get("verify")
+        email1= self.request.get("email")
+
+        if username == "":
+            error1= "Please enter a username."
+        else:
+            error1= ""
+        if password1 != verify1:
+            error2= "Passwords do not match"
+        else:
+            error2= ""
+        if not re.match("^[a-zA-Z0-9_-]{3,20}$", username):
+            error3="Please enter a valid username"
+        name_form_response="""
+        <form action= "/name" method= "post">
+            <label for="username">Username</label>
+                <input name="name" type="text">
+                <br>
+            <label for="password">Password</label>
+                <input name="password" type="password">
+                <br>
+            <label for="verify">Verify Password</label>
+                <input name="verify" type="password">
+                <br>
+            <label for="email">Email(Optional)</label>
+                <input name="email" type="email">
+                <br>
+                <input type= "submit">
+                <br>
+                <p class="error">""" + error1 + error2 + error3 +  """</p>
+        </form>
+        """
+        self.response.write(page_header + name_form_response + page_footer)
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/name', Response)
 ], debug=True)
